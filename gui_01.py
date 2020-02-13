7027f4f145649e601a17b2e01b2e5a53827d469b
zixuan <2696055878@qq.com>
2020-02-13 19:42:27 +0800

    测试

---
+++ gui_01.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter import *
+
+tk = Tk()
+tk.title("测试GUI")
+tk.geometry("500x200+200+50")
+# lable = tkinter.Label(tk,
+#                       text="这是一个测试程序",
+#                       bg="pink", fg="red",
+#                       font=("黑体", 14),
+#                       width=14,
+#                       height=7,
+#                       wraplength=100,
+#                       justify="left",
+#                       anchor="ne")
+butten1 = tk.(tk, text="开始")
+butten1.pack()
+tk.mainloop()
